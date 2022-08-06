719fdb4fb657ae71ebbbed8cd985580c182c4853
vits56 <bastosduartebruno@gmail.com>
2022-08-05 22:52:18 -0300

    lógica de criptografia e descriptografia

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+const text = document.querySelector("textarea").value;
+const outText = document.getElementById("out-text");
+const inputText = document.querySelector("input-text");
 
+function changeText() {
+  const text = document.querySelector("textarea").value;
+  const outText = document.getElementById("out-text");
+
+  outText.innerHTML = text;
+}
+
+function cryptography() {
+  const text = document.querySelector("textarea").value;
+  const secondText = document.getElementById("out-text");
+
+  const resultCript = text.replace(/[e]/gi, 'enter').replace(/[i]/gi, 'imes').replace(/[a]/gi, 'ai').replace(/[o]/gi, 'ober').replace(/[u]/gi, 'ufat');
+  secondText.innerHTML = resultCript;
+}
+
+function decryption() {
+  const secondText = document.getElementById("out-text").value;
+  const outText = document.getElementById("out-text");
+
+  const resultDes = secondText.replace(/enter/gi, 'e').replace(/imes/gi, 'i').replace(/ai/gi, 'a').replace(/ober/gi, 'o').replace(/ufat/gi, 'u');
+
+  outText.innerHTML = resultDes;
+}
+
+function copy() {
+  const copyText = document.getElementById("out-text");
+
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+}
+
+function limpar() {
+  const text = document.querySelector("textarea").value;
+  const outText = document.getElementById("out-text");
+
+
+  outText.innerHTML = "";
+  text.innerHTML = "";
+}
